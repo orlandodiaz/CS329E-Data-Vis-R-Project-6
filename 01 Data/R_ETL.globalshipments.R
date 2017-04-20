82a69f3c53ddfeb5aa41516b843cf536c1318f44
ASUS\Franny <frannyfang@gmail.com>
2017-04-20 14:02:49 -0500

    no message

---
+++ 01 Data/R_ETL.globalshipments.R
@@ -0,0 +1,76 @@
+require(readr)
+require(plyr)
+
+# Download the pantheria file into a folder ../../CSVs and rename the file PreETL_Pantheria.csv
+file_path = "../../CSVs/PreETL_globalshipment.csv"
+globalshipment <- readr::read_csv(file_path)
+names(globalshipment)
+
+df <- globalshipment
+names(globalshipment)
+str(globalshipment) # Uncomment this line and  run just the lines to here to get column types to use for getting the list of measures.
+
+dimensions <- c("Order Date", "Ship Date", "Ship Mode", "Customer Name", "Segment", "City", "State", "Country", "Region", "Market", "Category", "Sub-Category", "Product Name", "Order Priority")
+dimensions
+
+measures <- setdiff(names(df), dimensions)
+measures
+
+# Get rid of special characters in each column.
+for(n in names(df)) {
+  df[n] <- data.frame(lapply(df[n], gsub, pattern="[^ -~]",replacement= ""))
+}
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# df["State"] <- data.frame(lapply(df["State"], toupper))
+
+na2emptyString <- function (x) {
+  x[is.na(x)] <- ""
+  return(x)
+}
+if( length(dimensions) > 0) {
+  for(d in dimensions) {
+    # Change NA to the empty string.
+    df[d] <- data.frame(lapply(df[d], na2emptyString))
+    # Get rid of " and ' in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
+    # Change & to and in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
+    # Change : to ; in dimensions.
+    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
+  }
+}
+
+na2zero <- function (x) {
+  x[is.na(x)] <- 0
+  return(x)
+}
+# Get rid of all characters in measures except for numbers, the - sign, and period.dimensions, and change NA to 0.
+if( length(measures) > 1) {
+  for(m in measures) {
+    print(m)
+    df[m] <- data.frame(lapply(df[m], gsub, pattern="[^--.0-9]",replacement= ""))
+    df[m] <- data.frame(lapply(df[m], gsub, pattern="[-999]", replacement="0"))
+    df[m] <- data.frame(lapply(df[m], na2zero))
+    df[m] <- lapply(df[m], function(x) as.numeric(as.character(x))) # This is needed to turn measures back to numeric because gsub turns them into strings.
+  }
+}
+str(df)
+
+write.csv(df, gsub("PreETL_", "", file_path), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 0 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 0 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
